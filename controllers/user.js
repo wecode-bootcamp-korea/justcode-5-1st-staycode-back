7b39787e0dcdc45bb4e6cd1258b2ed52f5598a9c
JAEHOON-1993 <ferodemic@sogang.ac.kr>
2022-06-30 19:06:47 +0900

    Add: 백엔드 최종

--- controllers/user.js
+++ controllers/user.js
@@ -1,30 +1,24 @@
 const { signup } = require('../services/user');
 const { login } = require('../services/user');
-const signupController = async(req, res)=>{
-    const { email, password,name, phone} = req.body;
+const signupController = async (req, res) => {
+  const { email, password, name, phone } = req.body;
 
-    await signup(email, password,name,phone);
-    
-    res.status(201).json({ message: 'SUCCESS'});
+  await signup(email, password, name, phone);
 
-}
+  res.status(201).json({ message: 'SUCCESS' });
+};
 
+const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
 
-
-
-const loginController = async (req, res, next)=>{
-    const { email, password} = req.body;
-  
-  try{
-  const token = await login(email, password);
-   res.json({token});
-  } catch(err){
-      const error = new Error("에러가 났습니다.")
-      error.statusCode = 400;
-      next(error);
+  try {
+    const token = await login(email, password);
+    res.json({ token });
+  } catch (err) {
+    const error = new Error('에러가 났습니다.');
+    error.statusCode = 400;
+    next(error);
   }
-
-   
-}
+};
 
 module.exports = { signupController, loginController };
