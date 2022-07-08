31d480de57774f3353a409dc14961d807743cb5a
유관희 <kwan@yugwanhuiui-MacBookPro.local>
2022-07-08 15:30:06 +0900

    jwt,verify를 이용해 user_id

---
+++ middlewares/validateToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const validateToken = async (req, res, next) => {
+    try{
+        const token = req.header('Authorization');
+        console.log(token);
+
+        const user = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(user);
+        req.userId = user.id;
+        next();
+    } catch (err){
+        next(err);
+    }
+};
+
+module.exports = { validateToken };
